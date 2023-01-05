a6a688c7f4252df46edebae5f3286e34e30247c9
Manuel Lamas <moncholamas@gmail.com>
2023-01-05 17:05:23 -0300

    Updates

--- src/config/axiosReq.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-import url from './urlApi'
-
-// crea una instancia de axios con la url de la api como base
-const axiosReq = axios.create({
-    baseURL: url
-})
-
-export default axiosReq
--- src/config/urlApi.js
+++
@@ -1,3 +0,0 @@
-const url = process.env.NODE_ENV === 'developm' ? 'http://localhost:8000/api' : "https://apimovie-production.up.railway.app/api";
-
-export default url;
