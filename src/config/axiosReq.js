f2c877d5457e1711c90c6d59f88c26a054fffacb
Manuel Lamas <moncholamas@gmail.com>
2023-01-05 17:01:13 -0300

    feat: gh-pages

--- src/config/axiosReq.js
+++ src/config/axiosReq.js
@@ -3,8 +3,7 @@ import url from './urlApi'
 
 // crea una instancia de axios con la url de la api como base
 const axiosReq = axios.create({
-    baseURL: url,
-    withCredentials: true
+    baseURL: url
 })
 
 export default axiosReq
