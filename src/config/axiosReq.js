a57fec9ee72620b6cdf7baaf974dc5f673ea05a3
Manuel Lamas <moncholamas@gmail.com>
2023-01-06 10:52:49 -0300

    fix: corrige la config de axios

--- src/config/axiosReq.js
+++ src/config/axiosReq.js
@@ -1,9 +1,12 @@
 import axios from 'axios'
 import url from './urlApi'
 
+const token = localStorage.getItem('token') || '';
+
 // crea una instancia de axios con la url de la api como base
 const axiosReq = axios.create({
-    baseURL: url
+    baseURL: url,
+    headers: { Authorization: `Bearer ${token}` }
 })
 
 export default axiosReq
